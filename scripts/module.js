72e1405fff50309e3c2f4823e471ef33ade89978
Michael Enion <caewok_2000@yahoo.com>
2021-11-14 16:33:28 -0800

    Add DevMode switch and log function.

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,24 @@
-Hooks.once('init', async function() {
+export const MODULE_ID = `lightmask`;
 
-});
 
-Hooks.once('ready', async function() {
+/**
+ * Log message only when debug flag is enabled from DevMode module.
+ * @param {Object[]} args  Arguments passed to console.log.
+ */
+export function log(...args) {
+  try (
+    const isDebugging = game.modules.get(`_dev_mode`)?.api?.getPackageDebugValue(MODULE_ID);
+    if( isDebugging ) {
+      console.log(MODULE_ID, `|`, ...args);
+    }
+    catch (e) {};
+  )
+}
 
+/**
+ * Tell DevMode that we want a flag for debugging this module.
+ * https://github.com/League-of-Foundry-Developers/foundryvtt-devMode
+ */
+Hooks.once(`devModeReady`, ({ registerPackageDebugFlag }) => {
+  registerPackageDebugFlag(MODULE_ID);
 });

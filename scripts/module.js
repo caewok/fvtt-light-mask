0ee05548bca3a498a8dade5ce5c0317455659065
Michael Enion <caewok_2000@yahoo.com>
2021-11-16 15:21:36 -0800

    Add a hook to modify the light configuration.

--- scripts/module.js
+++ scripts/module.js
@@ -22,3 +22,31 @@ export function log(...args) {
 Hooks.once(`devModeReady`, ({ registerPackageDebugFlag }) => {
   registerPackageDebugFlag(MODULE_ID);
 });
+
+
+/**
+ * Add a selector to the ambient light configuration
+ * templates/scene/ambient-light-config.html
+ * follows approach in https://github.com/erithtotl/wall-height/blob/master/scripts/wall-height.js
+ */
+Hooks.on("renderAmbientLightConfig", (app, html, data) => {
+  
+  const moduleLabel = MODULE_ID;
+  const pullDownLabel = `Shape`;
+  const SHAPE_KEY = "Shape"
+  const shape = 1;
+  
+  // insert in the advanced tab after the last group
+  // advanced tab is last tab
+  html.find(".form-group").last().after(`
+  <fieldset>
+      <legend>${moduleLabel}</legend>
+          <div class="form-group">
+              <label>${pullDownLabel}</label>
+              <input name="flags.${MODULE_ID}.${SHAPE_KEY}" type="text" data-dtype="Number" value="${shape}">
+          </div>
+      </legend>
+  </fieldset>
+  `); 
+    
+});

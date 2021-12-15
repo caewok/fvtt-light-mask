bdb07fa24e493d10e4360d2f9bd2ea0477a643c5
Michael Enion <caewok_2000@yahoo.com>
2021-12-15 13:39:58 -0800

    Add logic to update the relative origin for cached walls

--- scripts/const.js
+++ scripts/const.js
@@ -8,3 +8,5 @@ export const CUSTOM_IDS_KEY = "customWallIDs";
 export const CUSTOM_EDGES_KEY = "customEdges";
 export const CUSTOM_IDS_BUTTON_KEY = "customWallIDsButton";
 export const ROTATION_KEY = "rotation"; // for Sounds
+export const RELATIVE_KEY = "relative";
+export const ORIGIN_KEY = "origin";

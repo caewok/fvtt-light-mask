63e2a271a76df975eadc5f76ad4a4981d9038716
Michael Enion <caewok_2000@yahoo.com>
2022-05-09 21:40:41 -0700

    Update hint for setting re custom sweep

--- scripts/settings.js
+++ scripts/settings.js
@@ -15,8 +15,8 @@ export function registerSettings() {
   log("Registering light mask settings");
 
   game.settings.register(MODULE_ID, "use-lightmask-sweep-always", {
-    name: "Use Light Mask sweep algorithm everywhere",
-    hint: "Use Light Mask instead of Foundry default for all vision/light/sound field-of-view. May improve performance in some instances.",
+    name: "Always Use Light Mask sweep",
+    hint: "Use Light Mask instead of Foundry default algorithm for all vision/light/sound field-of-view. May improve performance in some instances but may also introduce compatibility issues with some modules.",
     scope: "world",
     config: true,
     default: false,

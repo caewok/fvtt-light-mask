60128ccaf0cd0993fd0131142df3a89fa0976dc0
Michael Enion <caewok_2000@yahoo.com>
2022-05-20 10:48:53 -0700

    Fix ellipse rotation and point creation

--- scripts/Ellipse.js
+++ scripts/Ellipse.js
@@ -40,14 +40,14 @@ export class Ellipse {
    */
   getBounds() {
     const m2 = this.major * 2;
-    if(rotation === 0 || rotation === 180) {
+    if(this.rotation === 0 || this.rotation === 180) {
       // Rectangle measured from top left corner. x, y, width, height
-      return PIXI.Rectangle(this.x - this.major, this.y - this.minor, m2, this.minor * 2);
+      return new PIXI.Rectangle(this.x - this.major, this.y - this.minor, m2, this.minor * 2);
     }
 
-    if(rotation === 90 || rotation === 270) {
+    if(this.rotation === 90 || this.rotation === 270) {
       // Rectangle measured from top left corner. x, y, width, height
-      return PIXI.Rectangle(this.x - this.minor, this.y - this.major, this.minor * 2, m2);
+      return new PIXI.Rectangle(this.x - this.minor, this.y - this.major, this.minor * 2, m2);
     }
 
     // Default to bounding box of the radius circle
@@ -94,11 +94,53 @@ export class Ellipse {
  *points({ density = 60 } = {}) {
    // See https://www.geeksforgeeks.org/how-to-discretize-an-ellipse-or-circle-to-a-polygon-using-c-graphics/
    const pi2 = Math.PI * 2;
-   let angle_shift = pi2 / density;
+   const rot = this.rotation;
+
+   // translation matrix:
+   // 1 0 this.x
+   // 0 1 this.y
+   // 0 0 1
+
+   // rotation matrix, where Ø = this.rotation
+   //  cos(Ø) sin(Ø) 0
+   // -sin(Ø) cos(Ø) 0
+   //  0      0      1
+
+   // u = TRv
+   // v = {x, y, 1}
+
+   // Translation * Rotation matrices
+   // 1 * cos(Ø) + 0 * -sin(Ø) + this.x * 0   1 * sin(Ø) + 0 * cos(Ø) + this.x * 0   1 * 0 + 0 * 0 + this.x * 1
+   // 0 * cos(Ø) + 1 * -sin(Ø) + this.y * 0   0 * sin(Ø) + 1 * cos(Ø) + this.y * 0   0 * 0 + 1 * 0 + this.y * 1
+   // 0 * cos(Ø) + 0 * -sin(Ø) + 1 * 0        0 * sin(Ø) + 0 * cos(Ø) + 1 * 0        0 * 0 + 0 * 0 + 1 * 1
+   // -->
+   //  cos(Ø)   sin(Ø)   this.x
+   // -sin(Ø)   cos(Ø)   this.y
+   //  0        0        1
+
+   // Times [x, y, 1]
+   // cos(Ø) * x + sin(Ø) * y + this.x * 1
+   // -sin(Ø) * x + cos(Ø) * y + this.y * 1
+   // 0 * x + 0 * y + 1 * 1
+   // -->
+   // cos(Ø) * x +  sin(Ø) * y + this.x
+   // -sin(Ø) * x + cos(Ø) * y + this.y
+   // 1
+
+   const cos_rot = Math.cos(Math.toRadians(rot));
+   const sin_rot = Math.sin(Math.toRadians(rot));
+
+   let angle_shift = pi2 / density; // convert angle shift to radians
    let phi = 0;
    for ( let i = 0; i < density; i += 1 ) {
+     const x = this.major * Math.cos(phi);
+     const y = this.minor * Math.sin(phi);
+
+     const xt = cos_rot * x + sin_rot * y + this.x;
+     const yt = -sin_rot * x + cos_rot * y + this.y;
+
+     yield {x: xt, y: yt};
      phi += angle_shift;
-     yield { x: this.major * Math.cos(phi), y: this.minor * Math.sin(phi) };
    }
  }
 

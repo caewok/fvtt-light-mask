7e6c8fd3bf24a119e23675eca51554e6efd257e4
Michael Enion <caewok_2000@yahoo.com>
2022-07-26 03:16:00 -0700

    Use custom lineCircleIntersection methods

--- scripts/shapes/PIXICircle.js
+++ scripts/shapes/PIXICircle.js
@@ -32,7 +32,7 @@ export function registerPIXICircleMethods() {
    */
   Object.defineProperty(PIXI.Circle.prototype, "segmentIntersections", {
     value: function(a, b) {
-      const ixs = foundry.utils.lineCircleIntersection(a, b, this, this.radius);
+      const ixs = lineCircleIntersection(a, b, this, this.radius);
       return ixs.intersections;
     },
     writable: true,
@@ -141,8 +141,6 @@ function pointsForArc(fromAngle, toAngle, {density, includeEndpoints=true} = {})
 }
 
 function intersectPolygonPIXICircle(wrapped, polygon, options) {
-  return wrapped(polygon, options);
-
   if ( !this.radius ) return new PIXI.Polygon([]);
   options.clipType ??= ClipperLib.ClipType.ctIntersection;
 
@@ -162,3 +160,97 @@ function intersectPolygonPIXICircle(wrapped, polygon, options) {
 
   return res instanceof PIXI.Polygon ? res : res.toPolygon();
 }
+
+// Needed to change 1 line in the quadraticIntersection, but cannot override, so...
+// May as well trim down lineCircleIntersection a bit while we are at it...
+/**
+ * Determine the intersection between a candidate wall and the circular radius of the polygon.
+ * @memberof helpers
+ *
+ * @param {Point} a                   The initial vertex of the candidate edge
+ * @param {Point} b                   The second vertex of the candidate edge
+ * @param {Point} center              The center of the bounding circle
+ * @param {number} radius             The radius of the bounding circle
+ * @param {number} epsilon            A small tolerance for floating point precision
+ *
+ * @returns {LineCircleIntersection}  The intersection of the segment AB with the circle
+ */
+function lineCircleIntersection(a, b, center, radius, epsilon=1e-8) {
+  const r2 = Math.pow(radius, 2);
+  let intersections = [];
+
+  // Test whether endpoint A is contained
+  const ar2 = Math.pow(a.x - center.x, 2) + Math.pow(a.y - center.y, 2);
+  const aInside = ar2 <= r2 + epsilon;
+
+  // Test whether endpoint B is contained
+  const br2 = Math.pow(b.x - center.x, 2) + Math.pow(b.y - center.y, 2);
+  const bInside = br2 <= r2 + epsilon;
+
+  // Find quadratic intersection points
+  const contained = aInside && bInside;
+  if ( !contained ) {
+    intersections = quadraticIntersection(a, b, center, radius, epsilon);
+  }
+
+  // Return the intersection data
+  return {
+    aInside,
+    bInside,
+    contained,
+    intersections
+  };
+}
+
+
+/**
+ * Determine the points of intersection between a line segment (p0,p1) and a circle.
+ * There will be zero, one, or two intersections
+ * See https://math.stackexchange.com/a/311956
+ * @memberof helpers
+ *
+ * @param {Point} p0            The initial point of the line segment
+ * @param {Point} p1            The terminal point of the line segment
+ * @param {Point} center        The center of the circle
+ * @param {number} radius       The radius of the circle
+ * @param {number} [epsilon=0]  A small tolerance for floating point precision
+ */
+function quadraticIntersection(p0, p1, center, radius, epsilon=0) {
+  const dx = p1.x - p0.x;
+  const dy = p1.y - p0.y;
+  // Quadratic terms where at^2 + bt + c = 0
+  const a = Math.pow(dx, 2) + Math.pow(dy, 2);
+  const b = (2 * dx * (p0.x - center.x)) + (2 * dy * (p0.y - center.y));
+  const c = Math.pow(p0.x - center.x, 2) + Math.pow(p0.y - center.y, 2) - Math.pow(radius, 2);
+
+  // Discriminant
+  const disc2 = Math.pow(b, 2) - (4 * a * c);
+  if ( disc2 < 0 ) return []; // No intersections
+
+  // Roots
+  const disc = Math.sqrt(disc2);
+  const t1 = (-b - disc) / (2 * a);
+  const t2 = (-b + disc) / (2 * a);
+  // If t1 hits (between 0 and 1) it indicates an "entry"
+  const intersections = [];
+  if ( t1.between(0-epsilon, 1+epsilon) ) {
+    intersections.push({
+      x: p0.x + (dx * t1),
+      y: p0.y + (dy * t1)
+    });
+  }
+
+  // If the discriminant is exactly 0, a segment endpoint touches the circle
+  // (and only one intersection point)
+  if ( disc2 === 0 ) return intersections;
+
+  // If t2 hits (between 0 and 1) it indicates an "exit"
+  if ( t2.between(0-epsilon, 1+epsilon) ) {
+    intersections.push({
+      x: p0.x + (dx * t2),
+      y: p0.y + (dy * t2)
+    });
+  }
+  return intersections;
+}
+

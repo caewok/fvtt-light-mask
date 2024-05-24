2a07f883975543fce6ae1a251e39b75e549e7a69
Michael Enion <caewok_2000@yahoo.com>
2024-05-23 22:24:31 -0700

    🐛 fix|LightConfig|Add module tab to light config

--- scripts/AmbientLightConfig.js
+++ scripts/AmbientLightConfig.js
@@ -5,7 +5,7 @@ Hooks
 /* eslint no-unused-vars: ["error", { "argsIgnorePattern": "^_" }] */
 "use strict";
 
-import { MODULE_ID, TEMPLATES, MODULE_ICON } from "./const.js";
+import { MODULE_ID, TEMPLATES, ICONS, SHAPE } from "./const.js";
 import { injectConfiguration, activateListeners } from "./render.js";
 
 // Patches for the AmbientSoundConfig class
@@ -14,25 +14,13 @@ PATCHES.BASIC = {};
 
 // Hook init to update the PARTS of the light config
 Hooks.once("init", function() {
-  foundry.applications.sheets.AmbientLightConfig.PARTS[MODULE_ID] = {
-    template: TEMPLATES.LIGHT
+  const { footer, ...other } = foundry.applications.sheets.AmbientLightConfig.PARTS;
+  foundry.applications.sheets.AmbientLightConfig.PARTS = {
+    other, // Includes tabs
+    [MODULE_ID]: { template: TEMPLATES.LIGHT },
+    footer
   }
 });
-
-
-// ----- NOTE: Hooks ----- //
-
-/**
- * @param {Application} application     The Application instance being rendered
- * @param {jQuery} html                 The inner HTML of the document that will be displayed and may be modified
- * @param {object} data                 The object of data used when rendering the application
- */
-function renderAmbientLightConfig(app, html, data) {
-  injectConfiguration(app, html, data, "LIGHT"); // Async
-  activateListeners(app, html);
-}
-
-PATCHES.BASIC.HOOKS = { renderAmbientLightConfig };
 
 // ----- NOTE: WRAPS ----- //
 
@@ -40,11 +28,11 @@ PATCHES.BASIC.HOOKS = { renderAmbientLightConfig };
  * Add additional module tab to the config.
  */
 async function _prepareContext(wrapped, options) {
-  const context = wrapped(options);
+  const context = await wrapped(options);
   context.tabs[MODULE_ID] =  {
     id: MODULE_ID,
     group: "sheet",
-    icon: MODULE_ICON,
+    icon: ICONS.MODULE,
     label: "lightmask.AmbientConfiguration.LegendTitle" };
 
   // From #getTabs
@@ -52,8 +40,26 @@ async function _prepareContext(wrapped, options) {
     v.active = this.tabGroups[v.group] === v.id;
     v.cssClass = v.active ? "active" : "";
   }
+
   return context;
 }
 
-PATCHES.BASIC.WRAPS = { _prepareContext };
+/**
+ * Add in lightmask specific data to the lightmask tab.
+ * @param {string} partId                         The part being rendered
+ * @param {ApplicationRenderContext} context      Shared context provided by _prepareContext
+ * @param {HandlebarsRenderOptions} options       Options which configure application rendering behavior
+ * @returns {Promise<ApplicationRenderContext>}   Context data for a specific part
+ */
+async function _preparePartContext(wrapped, partId, context, options) {
+  context = await wrapped(partId, context, options);
+  if ( partId !== MODULE_ID ) return context;
+  // Add in shapes
+  context[MODULE_ID] = {
+    shapes: SHAPE.LABELS
+  }
+  return context;
+}
+
+PATCHES.BASIC.WRAPS = { _prepareContext, _preparePartContext };
 
